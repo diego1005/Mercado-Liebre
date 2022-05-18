0c520a8c49b52f45a6c44f157df1c242cb05ee68
diego1005 <dmaguilar82@gmail.com>
2022-05-18 10:55:28 -0300

    make responsive website

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 //Routes
 app.get("/", (req, res) => {
     pathHome = path.join(__dirname, "views/index.html");
@@ -12,6 +14,6 @@ app.get("/", (req, res) => {
 app.use(express.static(path.join(__dirname, "public")));
 
 //Server
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 })
